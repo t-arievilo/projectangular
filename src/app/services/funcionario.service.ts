f323cef49b5d29eda9d12f8e1d0f90fde360ebac
thiago <thiago.math@hotmail.com>
2025-05-05 12:32:05 -0300

    connect to backend

---
+++ src/app/services/funcionario.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Observable } from 'rxjs';
+import { Funcionario } from '../models/Funcionarios';
+import { Response } from '../models/Response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  private apiUrl = '${environment.ApiUrl}/Funcionario';
+
+  constructor( private http: HttpClient ) { }
+
+  GetFuncionarios() : Observable<Response<Funcionario[]>> {
+    return this.http.get<Response<Funcionario[]>>(this.apiUrl)
+}
+}
